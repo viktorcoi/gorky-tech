612af756ba354ab40484d0443f60051acaa6cd18
viktorcoi <marandin5913@mail.ru>
2023-05-04 00:47:19 +0300

    2 blocks

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,36 +1,13 @@
 document.addEventListener('DOMContentLoaded', ()  => {
 
-    const header = document.querySelector('header')
-    const languageSelector = document.querySelector('#language-selector');
+    let slider = tns({
+        container: '.main-slider',
+        edgePadding: 158,
+        slideBy: 'page',
+        fixedWidth: 880,
+        nav: false,
+        prevButton: '#main-slider-prew',
+        nextButton: '#main-slider-next',
+    });
 
-    document.querySelector('#btn-open-menu').addEventListener('click', () => {
-        header.classList.toggle('open-menu')
-    })
-
-    document.querySelector('#switch-theme-dark').addEventListener('click', () => {
-        document.querySelector('#switcher-theme').checked = false;
-        document.querySelector('body').setAttribute('data-theme', 'dark');
-    })
-
-    document.querySelector('#switch-theme-light').addEventListener('click', () => {
-        document.querySelector('#switcher-theme').checked = true;
-        document.querySelector('body').setAttribute('data-theme', 'light');
-    })
-
-    languageSelector.querySelector('#language-selector-head').addEventListener('click', function() {
-        languageSelector.classList.toggle('open-language');
-    })
-
-    languageSelector.querySelector('#language-options').querySelectorAll('p').forEach((item) => {
-        item.addEventListener('click', function() {
-            languageSelector.classList.remove('open-language');
-            languageSelector.querySelector('#language-selector-head').querySelector('p').textContent = this.textContent;
-        })
-    }) 
-
-    document.addEventListener('click', (e) => {
-        const selectorLanguage = e.target.closest('#language-selector');
-        if (!selectorLanguage)
-        languageSelector.classList.remove('open-language');
-    })
 });

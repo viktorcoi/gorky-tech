dba4b379c24d787e84c2e12774568d7026712f5e
vitek <marandin5913@mail.ru>
2023-05-04 14:15:58 +0300

    test

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,9 +3,26 @@ document.addEventListener('DOMContentLoaded', ()  => {
     const header = document.querySelector('header')
     const languageSelector = document.querySelector('#language-selector');
 
+    const listHeaderDrops = ['open-search', 'open-menu'];
+
+    const closeDropHeader = (className) => {
+        listHeaderDrops.forEach(item => {
+            if (!header.classList.contains(className))
+            header.classList.remove(item)
+        })
+        header.classList.toggle(className)
+        header.classList.contains(className) ?
+        document.querySelector('body').classList.add('block-scroll') :
+        document.querySelector('body').classList.remove('block-scroll');
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
     document.querySelector('#btn-open-menu').addEventListener('click', () => {
-        header.classList.toggle('open-menu')
-        document.querySelector('body').classList.toggle('block-scroll');
+        closeDropHeader('open-menu');
+    })
+
+    document.querySelector('#header-btn-search').addEventListener('click', () => {
+        closeDropHeader('open-search');
     })
 
     document.querySelector('#switch-theme-dark').addEventListener('click', () => {
@@ -31,12 +48,21 @@ document.addEventListener('DOMContentLoaded', ()  => {
 
     document.addEventListener('click', (e) => {
         const selectorLanguage = e.target.closest('#language-selector');
-        const mainHeader = e.target.closest('header');
+        const headerOverlay = e.target.closest('#header-overlay');
         if (!selectorLanguage)
         languageSelector.classList.remove('open-language');
-        if (!mainHeader) {
+        if (headerOverlay) {
             document.querySelector('body').classList.remove('block-scroll');
             header.classList.remove('open-menu')
+            header.classList.remove('open-search')
+        }
+    })
+
+    document.addEventListener('mousedown', (e) => {
+        const headerOverlay = e.target.closest('#header-overlay');
+        if (headerOverlay) {
+            document.querySelector('body').classList.remove('block-scroll');
+            header.classList.remove('open-search')
         }
     })
 });

8bac207c375d8749dcb2c8a2ed88face03902dd6
viktorcoi <marandin5913@mail.ru>
2023-05-04 03:38:18 +0300

    next footer

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', ()  => {
         edgePadding: 158,
         slideBy: 'page',
         fixedWidth: 880,
+        gutter: 32,
         nav: false,
         prevButton: '#main-slider-prew',
         nextButton: '#main-slider-next',

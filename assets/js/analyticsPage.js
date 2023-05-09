03f021987522f6469d277c0b4edcdc87bd16e3f3
viktorcoi <marandin5913@mail.ru>
2023-05-09 20:30:10 +0300

    app

--- assets/js/analyticsPage.js
+++ assets/js/analyticsPage.js
@@ -4,4 +4,10 @@ document.addEventListener('DOMContentLoaded', ()  => {
 
     paginationTags('#analytics-tags', '#analytics-tags-page-prew', '#analytics-tags-page-next', '#search-analytics-tags');
     
+
+
+    document.querySelector('#analysts-question').addEventListener('click', () => {
+        document.querySelector('#analytics-page-popups').classList.add('popup-ask-question')
+    })
+
 });

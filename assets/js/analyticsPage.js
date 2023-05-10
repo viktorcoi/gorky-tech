ac9ce9e459f467a9022318c306bc0829384f0e1a
vitek <marandin5913@mail.ru>
2023-05-10 10:02:14 +0300

    final

--- assets/js/analyticsPage.js
+++ assets/js/analyticsPage.js
@@ -2,12 +2,43 @@ import { paginationTags } from './paginationTags.js';
 
 document.addEventListener('DOMContentLoaded', ()  => {
 
-    paginationTags('#analytics-tags', '#analytics-tags-page-prew', '#analytics-tags-page-next', '#search-analytics-tags');
+    const clearInputs = (popup) => {
+        popup.querySelectorAll('input').forEach(input => {
+            input.value = '';
+        })
+        popup.querySelectorAll('textarea').forEach(input => {
+            input.value = '';
+        })
+    }
     
-
-
     document.querySelector('#analysts-question').addEventListener('click', () => {
-        document.querySelector('#analytics-page-popups').classList.add('popup-ask-question')
+        document.querySelector('#analytics-page-popups').classList.add('popup-ask-question');
+        document.querySelector('body').classList.add('block-scroll');
     })
+
+    document.querySelector('#order-certificate').addEventListener('click', () => {
+        document.querySelector('#analytics-page-popups').classList.add('popup-ask-question');
+        document.querySelector('body').classList.add('block-scroll');
+    })
+
+    document.querySelector('#ask-question-close').addEventListener('click', () => {
+        document.querySelector('#analytics-page-popups').classList.remove('popup-ask-question');
+        setTimeout(() => {
+            clearInputs(document.querySelector('#popup-ask-question'));
+            document.querySelector('body').classList.remove('block-scroll');
+        }, 300);
+    })
+
+    document.querySelector('#analytics-page-popups').addEventListener('mousedown', (e) => {
+        if (!e.target.closest('.popup')) {
+            document.querySelector('#analytics-page-popups').classList.remove('popup-ask-question');
+            setTimeout(() => {
+                clearInputs(document.querySelector('#popup-ask-question'));
+                document.querySelector('body').classList.remove('block-scroll');
+            }, 300);
+        }
+    })
+
+    paginationTags('#analytics-tags', '#analytics-tags-page-prew', '#analytics-tags-page-next', '#search-analytics-tags');
 
 });

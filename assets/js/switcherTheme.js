be848dacbde8e25e97e11b95452a57e5c7966c54
vitek <marandin5913@mail.ru>
2023-05-16 09:46:03 +0300

    switch-theme

--- assets/js/switcherTheme.js
+++ assets/js/switcherTheme.js
@@ -1,31 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
+const themeSwitcher = document.querySelector('#switcher-theme');
+const themeDarkBtn = document.querySelector('#switch-theme-dark');
+const themeLightBtn = document.querySelector('#switch-theme-light');
 
-    const themeSwitcher = document.querySelector('#switcher-theme');
-    const themeDarkBtn = document.querySelector('#switch-theme-dark');
-    const themeLightBtn = document.querySelector('#switch-theme-light');
-    
-    const handleThemeSwitch = () => {
-        const isDarkTheme = !themeSwitcher.checked;
-        document.body.setAttribute('data-theme', isDarkTheme ? 'dark' : 'light');
-        localStorage.setItem('isDarkTheme', isDarkTheme);
-    };
-    
-    themeDarkBtn.addEventListener('click', () => {
-        themeSwitcher.checked = false;
-        handleThemeSwitch();
-    });
-    
-    themeLightBtn.addEventListener('click', () => {
-        themeSwitcher.checked = true;
-        handleThemeSwitch();
-    });
-    
-    const savedTheme = localStorage.getItem('isDarkTheme');
-    if (savedTheme !== null) {
-        themeSwitcher.checked = savedTheme !== 'true';
-        handleThemeSwitch();
-    } else {
-        document.body.setAttribute('data-theme', 'dark');
+const handleThemeSwitch = () => {
+    const isDarkTheme = !themeSwitcher.checked;
+    document.body.setAttribute('data-theme', isDarkTheme ? 'dark' : 'light');
+    setCookie('site_theme', isDarkTheme ? 'dark' : 'light', 365);
+};
+
+themeDarkBtn.addEventListener('click', () => {
+    themeSwitcher.checked = false;
+    handleThemeSwitch();
+});
+
+themeLightBtn.addEventListener('click', () => {
+    themeSwitcher.checked = true;
+    handleThemeSwitch();
+});
+
+const savedTheme = getCookie('site_theme');
+if (savedTheme !== '') {
+    themeSwitcher.checked = savedTheme !== 'dark';
+}
+handleThemeSwitch();
+
+function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+    document.cookie = name + '=' + value + ';expires=' + expires.toUTCString() + ';path=/';
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
     }
+    return '';
+}
 
+document.body.classList.add('no-transition');
+
+window.addEventListener('load', () => {
+    document.body.classList.remove('no-transition');
 });

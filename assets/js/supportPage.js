e22280d3e86df0409c985fe882c24e2994cbadfb
viktorcoi <marandin5913@mail.ru>
2023-05-20 16:51:19 +0300

    it-support

--- assets/js/supportPage.js
+++ assets/js/supportPage.js
@@ -14,7 +14,31 @@ document.addEventListener('DOMContentLoaded', ()  => {
         document.querySelector('#support-question').addEventListener('click', () => {
             openPopup('popup-ask-question', '#support-page-popups');
         })
+
+        document.querySelector('#btn-bot-telegram').addEventListener('click', () => {
+            openPopup('popup-bot-telegram', '#support-page-popups');
+        })
+
+        document.querySelector('#btn-startup-nn').addEventListener('click', () => {
+            openPopup('popup-out-startup-nn', '#support-page-popups');
+        })
+
+        document.querySelector('#btn-preferential-lending').addEventListener('click', () => {
+            openPopup('popup-out-preferential-lending', '#support-page-popups');
+        })
     
+        document.querySelector('#bot-telegram-close').addEventListener('click', () => {
+            closePopup('popup-bot-telegram', '#support-page-popups');
+        })
+
+        document.querySelector('#out-preferential-lending-close').addEventListener('click', () => {
+            closePopup('popup-out-preferential-lending', '#support-page-popups');
+        })
+
+        document.querySelector('#out-startup-nn-close').addEventListener('click', () => {
+            closePopup('popup-out-startup-nn', '#support-page-popups');
+        })
+
         document.querySelector('#ask-question-close').addEventListener('click', () => {
             closePopup('popup-ask-question', '#support-page-popups', 'inputs-mask', phoneMask);
         })
@@ -22,9 +46,12 @@ document.addEventListener('DOMContentLoaded', ()  => {
         document.querySelector('#support-page-popups').addEventListener('mousedown', (e) => {
             if (!e.target.closest('.popup')) {
                 closePopup('popup-ask-question', '#support-page-popups', 'inputs-mask', phoneMask);
+                closePopup('popup-bot-telegram', '#support-page-popups');
+                closePopup('popup-out-startup-nn', '#support-page-popups');
+                closePopup('popup-out-preferential-lending', '#support-page-popups');
             }
         })
-    
+
         inputName.addEventListener('input', function() {
             fillInput(this);
         })

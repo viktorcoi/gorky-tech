989b2e4bcfae6936ae2e02ca54a87d8cf6618d5d
viktorcoi <marandin5913@mail.ru>
2023-05-20 16:59:03 +0300

    it-support

--- assets/js/supportPage.js
+++ assets/js/supportPage.js
@@ -11,45 +11,27 @@ document.addEventListener('DOMContentLoaded', ()  => {
         let phoneMask, maskOptions = { mask: '+{7} (000) 000-00-00' };
         phoneMask = IMask(inputPhone, maskOptions);
     
-        document.querySelector('#support-question').addEventListener('click', () => {
-            openPopup('popup-ask-question', '#support-page-popups');
-        })
-
         document.querySelector('#btn-bot-telegram').addEventListener('click', () => {
             openPopup('popup-bot-telegram', '#support-page-popups');
         })
 
-        document.querySelector('#btn-startup-nn').addEventListener('click', () => {
-            openPopup('popup-out-startup-nn', '#support-page-popups');
+        document.querySelector('#support-question').addEventListener('click', () => {
+            openPopup('popup-ask-question', '#support-page-popups');
         })
 
-        document.querySelector('#btn-preferential-lending').addEventListener('click', () => {
-            openPopup('popup-out-preferential-lending', '#support-page-popups');
-        })
-    
-        document.querySelector('#bot-telegram-close').addEventListener('click', () => {
-            closePopup('popup-bot-telegram', '#support-page-popups');
-        })
-
-        document.querySelector('#out-preferential-lending-close').addEventListener('click', () => {
-            closePopup('popup-out-preferential-lending', '#support-page-popups');
-        })
-
-        document.querySelector('#out-startup-nn-close').addEventListener('click', () => {
-            closePopup('popup-out-startup-nn', '#support-page-popups');
+        document.querySelector('#support-page-popups').addEventListener('mousedown', (e) => {
+            if (!e.target.closest('.popup')) {
+                closePopup('popup-ask-question', '#support-page-popups', 'inputs-mask', phoneMask);
+                closePopup('popup-bot-telegram', '#support-page-popups');
+            }
         })
 
         document.querySelector('#ask-question-close').addEventListener('click', () => {
             closePopup('popup-ask-question', '#support-page-popups', 'inputs-mask', phoneMask);
         })
     
-        document.querySelector('#support-page-popups').addEventListener('mousedown', (e) => {
-            if (!e.target.closest('.popup')) {
-                closePopup('popup-ask-question', '#support-page-popups', 'inputs-mask', phoneMask);
-                closePopup('popup-bot-telegram', '#support-page-popups');
-                closePopup('popup-out-startup-nn', '#support-page-popups');
-                closePopup('popup-out-preferential-lending', '#support-page-popups');
-            }
+        document.querySelector('#bot-telegram-close').addEventListener('click', () => {
+            closePopup('popup-bot-telegram', '#support-page-popups');
         })
 
         inputName.addEventListener('input', function() {
@@ -82,5 +64,29 @@ document.addEventListener('DOMContentLoaded', ()  => {
         })
     }
 
+    if (document.querySelector('.support-page')) {
+        document.querySelector('#btn-startup-nn').addEventListener('click', () => {
+            openPopup('popup-out-startup-nn', '#support-page-popups');
+        })
+
+        document.querySelector('#btn-preferential-lending').addEventListener('click', () => {
+            openPopup('popup-out-preferential-lending', '#support-page-popups');
+        })
+
+        document.querySelector('#out-preferential-lending-close').addEventListener('click', () => {
+            closePopup('popup-out-preferential-lending', '#support-page-popups');
+        })
+
+        document.querySelector('#out-startup-nn-close').addEventListener('click', () => {
+            closePopup('popup-out-startup-nn', '#support-page-popups');
+        })
+
+        document.querySelector('#support-page-popups').addEventListener('mousedown', (e) => {
+            if (!e.target.closest('.popup')) {
+                closePopup('popup-out-startup-nn', '#support-page-popups');
+                closePopup('popup-out-preferential-lending', '#support-page-popups');
+            }
+        })
+    }
 
 })

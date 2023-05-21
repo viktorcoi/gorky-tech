5ab1608a1bdb45e0767e51f4e18a0d98a934d792
viktorcoi <marandin5913@mail.ru>
2023-05-21 15:47:50 +0300

    it-personnel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 // import './header.js'
 // import './switcherTheme.js'
-import './functions.js'
+// import './functions.js'
 // import './paginationTags.js'
 
 
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/header.js';
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/switcherTheme.js';
-// import 'https://viktorcoi.github.io/gorky-tech/assets/js/functions.js';
+import 'https://viktorcoi.github.io/gorky-tech/assets/js/functions.js';
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/paginationTags.js';

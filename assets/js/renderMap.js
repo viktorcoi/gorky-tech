c1e044cc71bf7c358a7a35c22806f6554f84f6c0
viktorcoi <marandin5913@mail.ru>
2023-05-28 22:07:37 +0300

    map

--- assets/js/renderMap.js
+++ assets/js/renderMap.js
@@ -1,40 +1,24 @@
 import { getCookie } from './functions.js';
 
-
-const language = getCookie('site_language');
- 
-    let script = document.createElement('script');
-    script.src = `https://api-maps.yandex.ru/2.1/?lang=${language === 'ru' ? 'ru_RU' : 'en_US'}&amp;apikey=b535750c-712d-4483-bfd8-68f0dfb1e89a`;
-    document.body.appendChild(script);
-
 document.addEventListener('DOMContentLoaded', ()  => {
-    let icon_type;
-    let myPlacemark;
-
     if(document.querySelector("#contacts-map")) {
-
+        const language = getCookie('site_language');
+        let script = document.createElement('script');
+        script.src = `https://api-maps.yandex.ru/2.1/?lang=${language === 'ru' ? 'ru_RU' : 'en_US'}&amp;apikey=b11312ae-7d16-42a3-a763-eca387d914d4`;
+        document.body.appendChild(script);
         script.onload = () => {
             ymaps.ready(renderMap);
         }
         const renderMap = () => {
-            let place_location = $('#contacts-map').attr('data-company-location').split(',');
-            let isgorkyai = $('#contacts-map').attr('isgorkyai');
-            if (typeof isgorkyai !== typeof undefined && isgorkyai !== false) {
-                icon_type = {
-                    iconLayout: 'default#image',
-                    iconImageHref: 'https://gorky.tech/assets/images/map_label.png',
-                    iconImageSize: [90, 60],
-                    iconImageOffset: [ -90/2 , -60]
-                }
-            } else {
-                icon_type = {
-                    preset: 'islands#governmentCircleIcon',
-                    iconColor: '#a33a8c'
-                }
-            }        
+            let icon_type = {
+                iconLayout: 'default#image',
+                iconImageHref: 'https://gorky.tech/assets/images/map_label.png',
+                iconImageSize: [90, 60],
+                iconImageOffset: [ -90/2 , -60]
+            }              
             let myMap = new ymaps.Map("contacts-map", 
             { 
-                center: place_location, 
+                center: [56.32537606277242, 44.01494270171355], 
                 zoom: 17, 
                 controls: []
             }, 
@@ -49,7 +33,7 @@ document.addEventListener('DOMContentLoaded', ()  => {
                 }       
             });
             myMap.controls.add(zoomControl);
-            myPlacemark = new ymaps.Placemark(place_location, {}, icon_type );
+            let myPlacemark = new ymaps.Placemark([56.32537606277242, 44.01494270171355], {}, icon_type );
             myMap.geoObjects.add(myPlacemark);
         }
     }

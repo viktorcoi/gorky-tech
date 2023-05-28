6de286219349bec4b15ba0b7ed9cac7e1bd4c05f
viktorcoi <marandin5913@mail.ru>
2023-05-28 06:38:59 +0300

    poster popup

--- assets/js/posterPage.js
+++ assets/js/posterPage.js
@@ -2,6 +2,8 @@ import { openPopup, closePopup } from './functions.js';
 
 document.addEventListener('DOMContentLoaded', ()  => {
 
+    const popupPoster = document.querySelector('#popup-poster-about');
+
     document.querySelector('#list-popups').addEventListener('mousedown', (e) => {
         if (!e.target.closest('.popup')) {
             closePopup('popup-poster-about', '#list-popups');
@@ -14,8 +16,28 @@ document.addEventListener('DOMContentLoaded', ()  => {
 
     document.addEventListener('click', (e) => {
         const showPopup = e.target.closest('.posters__block-button')
-        if (showPopup)
-        openPopup('popup-poster-about', '#list-popups');
+        if (showPopup) {
+            if (showPopup.closest('.posters__block').dataset.event) {
+                let data = JSON.parse(showPopup.closest('.posters__block').dataset.event.replace(/'/g, '"'));
+                popupPoster.querySelector('#image-poster').setAttribute('src', data?.image);
+                let blockNumber = popupPoster.querySelector('#number-poster');
+                blockNumber.querySelector('p').textContent = data?.day;
+                blockNumber.querySelector('span').textContent = data?.month;
+                let blockTime = popupPoster.querySelector('#time-poster');
+                blockTime.querySelector('p').textContent = data?.time;
+                blockTime.querySelector('span').textContent = data?.dow;
+                let blockDesc = popupPoster.querySelector('#desc-poster');
+                blockDesc.querySelector('p').textContent = data?.address !== '' ? data?.address : 'Онлайн мероприятие';
+                blockDesc.querySelector('a').textContent = data?.offline === '1' ? 'Показать на карте' : 'Ссылка на трансляцию';
+                blockDesc.querySelector('a').setAttribute('href', data?.offline === '1' ? `https://yandex.ru/maps/?text=${data?.address}` : data?.broadcast);
+                popupPoster.querySelectorAll('h2').forEach(item => {
+                    item.textContent = data?.name;
+                })
+                popupPoster.querySelector('#second-text-poster').innerHTML = data?.desc;
+                popupPoster.querySelector('#button-poster').textContent = data?.nameButton;
+                popupPoster.querySelector('#button-poster').setAttribute('href', data?.external);
+                openPopup('popup-poster-about', '#list-popups');
+            }
+        }
     })
-
 })

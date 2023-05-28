b722f74cc1ca3f72ece704d7a082e1b30c2edff7
viktorcoi <marandin5913@mail.ru>
2023-05-28 17:12:51 +0300

    fixes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,16 @@
 const fillInput = (input) => {
-    console.log(input.getAttribute('type'))
     if (input.value.trim().length < 1) {
         input.classList.add('error-input');
-        input.nextElementSibling.style.display = 'block';
-        input.nextElementSibling.classList.add('animate__animated', 'animate__fadeIn');
+        if (input.getAttribute('type') !== 'hidden') {
+            input.nextElementSibling.style.display = 'block';
+            input.nextElementSibling.classList.add('animate__animated', 'animate__fadeIn');
+        }
     } else {
         input.classList.remove('error-input');
-        input.nextElementSibling.style.display = 'none';
-        input.nextElementSibling.classList.remove('animate__animated', 'animate__fadeIn');   
+        if (input.getAttribute('type') !== 'hidden') {
+            input.nextElementSibling.style.display = 'none';
+            input.nextElementSibling.classList.remove('animate__animated', 'animate__fadeIn');   
+        }
     }
 }
 

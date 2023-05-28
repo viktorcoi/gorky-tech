2d20dc071ab8c1e574f14b5d0d1b0d59baad9981
viktorcoi <marandin5913@mail.ru>
2023-05-28 22:15:47 +0300

    map

--- assets/js/renderMap.js
+++ assets/js/renderMap.js
@@ -1,15 +1,21 @@
 import { getCookie } from './functions.js';
 
+
+const language = getCookie('site_language');
+
+let script = document.createElement('script');
+script.src = `https://api-maps.yandex.ru/2.1/?lang=${language === 'ru' ? 'ru_RU' : 'en_US'}&amp;apikey=b11312ae-7d16-42a3-a763-eca387d914d4`;
+document.body.appendChild(script);
+
 document.addEventListener('DOMContentLoaded', ()  => {
+
     if(document.querySelector("#contacts-map")) {
-        const language = getCookie('site_language');
-        let script = document.createElement('script');
-        script.src = `https://api-maps.yandex.ru/2.1/?lang=${language === 'ru' ? 'ru_RU' : 'en_US'}&amp;apikey=b11312ae-7d16-42a3-a763-eca387d914d4`;
-        document.body.appendChild(script);
+
         script.onload = () => {
             ymaps.ready(renderMap);
         }
         const renderMap = () => {
+            let place_location = $('#contacts-map')?.attr('data-company-location')?.split(',');
             let icon_type = {
                 iconLayout: 'default#image',
                 iconImageHref: 'https://gorky.tech/assets/images/map_label.png',
@@ -18,7 +24,7 @@ document.addEventListener('DOMContentLoaded', ()  => {
             }              
             let myMap = new ymaps.Map("contacts-map", 
             { 
-                center: [56.32537606277242, 44.01494270171355], 
+                center: place_location ? place_location : [56.32537606277242, 44.01494270171355], 
                 zoom: 17, 
                 controls: []
             }, 
@@ -33,7 +39,7 @@ document.addEventListener('DOMContentLoaded', ()  => {
                 }       
             });
             myMap.controls.add(zoomControl);
-            let myPlacemark = new ymaps.Placemark([56.32537606277242, 44.01494270171355], {}, icon_type );
+            let myPlacemark = new ymaps.Placemark(place_location ? place_location : [56.32537606277242, 44.01494270171355], {}, icon_type );
             myMap.geoObjects.add(myPlacemark);
         }
     }

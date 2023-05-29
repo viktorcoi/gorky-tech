877863f8fe0858b8bbd23ae963622cf8acf77be0
vitek <marandin5913@mail.ru>
2023-05-29 20:14:05 +0300

    js

--- assets/js/supportPage.js
+++ assets/js/supportPage.js
@@ -21,23 +21,23 @@ document.addEventListener('DOMContentLoaded', ()  => {
 
     if (document.querySelector('.support-page')) {
 
-        document.querySelector('#btn-startup-nn').addEventListener('click', () => {
+        document.querySelector('#btn-startup-nn')?.addEventListener('click', () => {
             openPopup('popup-out-startup-nn', '#list-popups-form');
         })
 
-        document.querySelector('#btn-preferential-lending').addEventListener('click', () => {
+        document.querySelector('#btn-preferential-lending')?.addEventListener('click', () => {
             openPopup('popup-out-preferential-lending', '#list-popups-form');
         })
 
-        document.querySelector('#out-preferential-lending-close').addEventListener('click', () => {
+        document.querySelector('#out-preferential-lending-close')?.addEventListener('click', () => {
             closePopup('popup-out-preferential-lending', '#list-popups-form');
         })
 
-        document.querySelector('#out-startup-nn-close').addEventListener('click', () => {
+        document.querySelector('#out-startup-nn-close')?.addEventListener('click', () => {
             closePopup('popup-out-startup-nn', '#list-popups-form');
         })
 
-        document.querySelector('#list-popups-form').addEventListener('mousedown', (e) => {
+        document.querySelector('#list-popups-form')?.addEventListener('mousedown', (e) => {
             if (!e.target.closest('.popup')) {
                 closePopup('popup-out-startup-nn', '#list-popups-form');
                 closePopup('popup-out-preferential-lending', '#list-popups-form');

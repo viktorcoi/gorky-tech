947dc1fab6150d4595d310f8eb879c58c966310d
vitek <marandin5913@mail.ru>
2023-06-07 12:58:12 +0300

    capcha

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 // import './header.js'
 // import './switcherTheme.js'
-import './functions.js'
+// import './functions.js'
 // import './paginationTags.js'
 // import './pagination.js'
-import './popups.js'
+// import './popups.js'
 // import './languageControl.js'
 
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/header.js';
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/switcherTheme.js';
-// import 'https://viktorcoi.github.io/gorky-tech/assets/js/functions.js';
+import 'https://viktorcoi.github.io/gorky-tech/assets/js/functions.js';
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/paginationTags.js';
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/pagination.js'
-// import 'https://viktorcoi.github.io/gorky-tech/assets/js/popups.js'
+import 'https://viktorcoi.github.io/gorky-tech/assets/js/popups.js'
 import 'https://viktorcoi.github.io/gorky-tech/assets/js/languageControl.js'
